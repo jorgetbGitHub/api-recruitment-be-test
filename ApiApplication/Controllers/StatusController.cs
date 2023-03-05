6052aa53b877f5460cfefb2ad77fd5bd08ee04c4
JorgeTB <jorgeTB@DESKTOP-TMBP506>
2023-03-05 19:07:13 +0100

    StatusController class has been created and its actions were defined.

---
+++ ApiApplication/Controllers/StatusController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ApiApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
