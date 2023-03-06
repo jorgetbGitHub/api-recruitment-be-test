5dc7b725e59b89e6b318f50c32f8ea613dce151f
JorgeTB <jorgeTB@DESKTOP-TMBP506>
2023-03-06 11:53:51 +0100

    IMDBStatus implements Singleton pattern.

--- ApiApplication/Database/Entities/IMDBStatus.cs
+++ ApiApplication/Database/Entities/IMDBStatus.cs
@@ -2,9 +2,24 @@
 
 namespace ApiApplication.Database.Entities
 {
-    public class IMDBStatus
+    public sealed class IMDBStatus
     {
         public bool Up { get; set; }
         public DateTime LastCall { get; set; }
+
+        private static readonly object _lock = new object();
+        private static IMDBStatus _instance = null;
+        public static IMDBStatus Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    _instance ??= new IMDBStatus();
+                }
+
+                return _instance;
+            }
+        }
     }
 }
